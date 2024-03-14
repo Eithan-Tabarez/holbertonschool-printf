c8365fca1a505e8f43e4dd9776bd04e247b34fd8
Eithan-Tabarez <146757670+Eithan-Tabarez@users.noreply.github.com>
2024-03-14 09:28:17 -0300

    printf

--- main.h
+++ main.h
@@ -5,8 +5,9 @@
 #include <stdio.h>
 
 void print_strings(const char *separator, const unsigned int n, ...);
-//int _printf(const char *format, ...);
-
+/**
+*int _printf(const char *format, ...);
+*/
 
 
 
