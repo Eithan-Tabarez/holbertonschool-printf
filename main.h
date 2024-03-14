fa936eac06f891eee62b46e616856140f8c271b0
Eithan-Tabarez <146757670+Eithan-Tabarez@users.noreply.github.com>
2024-03-14 09:07:40 -0300

    Printf

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+
+
+int _printf(const char *format, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+
+
+
+
+#endif
---
+++ print_string.c
@@ -0,0 +1,35 @@
+#incluide "main.h"
+
+/**
+* print_string - Print a string.
+*
+*/
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+    va_list strings;
+    char *str;
+    unsigned int i;
+
+    va_start(strings, n);
+
+    for (i = 0; i < n; i++)
+    {
+        str = va_arg(strings, char *);
+
+        if (str == NULL)
+         {
+            return (0);
+            str++;
+         }
+        else
+            printf("%s", str);
+
+        if (i != (n - 1) && separator != NULL)
+            printf("%s", separator);
+    }
+
+    printf("\n");
+
+    va_end(strings);
+}
